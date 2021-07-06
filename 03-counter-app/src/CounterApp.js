9ebdfe4f45ab824aaa03f0a3eb54b3f521435b40
Jose Erney Ospina <joseerney.ospina@globant.com>
2021-07-06 13:50:12 -0500

    end of section one

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ( { value} ) => {
+
+    const [ counter, setCounter ] = useState(value) // [] retorna un arreglo con dos valores
+
+    const handleAdd = () => {
+        // Manera 1 -> Mejor usar este si podemos
+        setCounter( counter + 1 )
+        // Manera 2 -> Algunas veces usaremos este
+        // setCounter( (c) => c + 1 );
+    }
+
+    const handleSubstract = () => {
+        setCounter( counter - 1 )
+    }
+
+    const handleReset= () => {
+        setCounter( value )
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
+
+            {/* <button onClick = { (e) => { handleAdd(e)}}> +1 </button> */}
+            <button onClick = { handleAdd }> +1 </button>
+            <button onClick = { handleReset }> Reset </button>
+            <button onClick = { handleSubstract }> -1 </button>
         </>
     )
 }
