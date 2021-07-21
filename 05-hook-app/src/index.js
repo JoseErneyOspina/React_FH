86952a847b711daaeb0b4decd1f56b9a6426c3a1
Jose Erney Ospina <joseerney.ospina@globant.com>
2021-07-21 16:37:56 -0500

    Multiple Hooks

---
+++ 05-hook-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import HookApp from './HookApp';
+// import { CounterApp } from './components/useState/CounterApp';
+// import { CounterWithCustomHook } from './components/useState/CounterWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+
+ReactDOM.render(
+    <MultipleCustomHooks />,
+  document.getElementById('root')
+);
+
