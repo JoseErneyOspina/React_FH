e5c2933f63fb7a39f248c2afbcb2ae3d44fbd0f3
Jose Erney Ospina <joseerney.ospina@globant.com>
2021-07-25 15:18:13 -0500

    CreateContext y useContext

---
+++ 05-hook-app/src/components/09-useContext/HomeScreen.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
+
+export const HomeScreen = () => {
+
+
+    const userContext = useContext(UserContext);
+    console.log(userContext);
+
+
+    return (
+        <>
+            <h1>HomeScreen</h1>
+            <hr />
+        </>
+    )
+}
---
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
+
+export const MainApp = () => {
+
+
+    const user = {
+        id: 1234,
+        name: 'Fernando',
+        email: 'fernando.herrera@gmail.com'
+    };
+
+
+    return (
+        <>
+            <UserContext.Provider value={ user }>
+
+                <AppRouter />
+
+            </UserContext.Provider>
+        </>
+    )
+}
