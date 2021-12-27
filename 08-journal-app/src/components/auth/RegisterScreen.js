6ef41cc8a4bb1beecca59205eb3ce7f4c012d197
Jose Erney Ospina <joseerneyosp@gmail.com>
2021-12-27 06:35:05 -0500

    ( Layout )--[ Auth ]-- Update Layout for Auth Components

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Sign Up</h3>
+            <form className="auth__form">
+                <input
+                    type="text"
+                    placeholder="Full Name"
+                    name="fullName"
+                    className="auth__input"
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    className="auth__input"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    className="auth__input"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    className="auth__input"
+                />
+                <button
+                    type="submit"
+                    className="btn btn-primary btn-block"
+                    // disabled={ true }
+                >
+                    Sign Up
+                </button>
+
+                <Link
+                    to="/auth/login"
+                    className="link"
+                >
+                    Already Registered?
+                </Link>
+            </form>
+        </>
     );
 };
